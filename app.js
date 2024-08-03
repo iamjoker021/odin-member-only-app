36039bad1800747b69b47b0c8183227a69fedab4
iamjoker021 <iamjoker021@gmail.com>
2024-08-03 17:11:20 +0530

    feat: Add session to App and store session in Postgres

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const dotenv = require('dotenv').config();
 const path = require('node:path');
-const express = require('express');
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
 const signupRouter = require('./router/signupRouter');
+const pool = require('./db/pool');
+const express = require('express');
+
 const app = express();
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1 * 60 * 60 * 1000
+    },
+    store: new pgSession({
+        pool: pool,
+        tableName: 'user_session',
+        createTableIfMissing: true
+    }),
+}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -11,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => res.send('Some Data'));
+app.get('/', (req, res) => res.render('index'));
 
 app.use('/sign-up', signupRouter);
 
